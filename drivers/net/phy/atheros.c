11f5fa7c27e990616c4f57a7f8ba9778fe2fc6bb
Jon Nettleton <jon@laptop.org>
2013-10-06 09:38:55 +0200

    phy: atheros: Add quirk to disable SmartEEE
    The Ar803x SmartEEE feature can cause a status glitch that
    causes an ethernet link up/down issue.  Allow it to be
    disabled on effected boards.

--- drivers/net/phy/atheros.c
+++ drivers/net/phy/atheros.c
@@ -20,6 +20,18 @@ static int ar8035_config(struct phy_device *phydev)
 {
 	int regval;
 
+#ifdef ATHEROS_QUIRK_SMARTEEE
+	/* Ar803x phy SmartEEE feature cause link status generates glitch,
+	 * which cause ethernet link down/up issue, so disable SmartEEE
+	 */
+	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x0003);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, 0x805d);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x4003);
+	regval = phy_read(phydev, MDIO_DEVAD_NONE, 0xe);
+	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, (regval&~0x0100));
+#endif
+
+	/* To enable AR8035 ouput a 125MHz clk from CLK_25M */
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x0007);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xe, 0x8016);
 	phy_write(phydev, MDIO_DEVAD_NONE, 0xd, 0x4007);
